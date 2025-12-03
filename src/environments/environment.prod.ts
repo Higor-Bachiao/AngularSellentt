426a3d2aa69eca7d85bc531ebfa856381f52b763
Higor <higor@sellentt.com.br>
2025-12-03 13:17:44 -0300

    inicio projeto angular

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+  apiKey: "AIzaSyD92QAVhgyd-eoP94KcLtBgmDb9BscXhpc",
+  authDomain: "codeiblesocialmediaproje-e4d8f.firebaseapp.com",
+  projectId: "codeiblesocialmediaproje-e4d8f",
+  storageBucket: "codeiblesocialmediaproje-e4d8f.firebasestorage.app",
+  messagingSenderId: "740757689132",
+  appId: "1:740757689132:web:af9ce8084da6e6df60d289",
+  measurementId: "G-3FZQGKD8N7"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+  apiKey: "AIzaSyD92QAVhgyd-eoP94KcLtBgmDb9BscXhpc",
+  authDomain: "codeiblesocialmediaproje-e4d8f.firebaseapp.com",
+  projectId: "codeiblesocialmediaproje-e4d8f",
+  storageBucket: "codeiblesocialmediaproje-e4d8f.firebasestorage.app",
+  messagingSenderId: "740757689132",
+  appId: "1:740757689132:web:af9ce8084da6e6df60d289",
+  measurementId: "G-3FZQGKD8N7"
+}
+
 };
 
 /*
