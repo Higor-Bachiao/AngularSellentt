f88ac2dd6bfc88aba1be4a8d2b422f52508bf4d7
Higor <higor@sellentt.com.br>
2025-12-05 09:52:10 -0300

    feito a tela de comentario save

--- src/app/tools/post/post.component.ts
+++ src/app/tools/post/post.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {PostData} from 'src/app/pages/post-feed/post-feed.component';
+import {FirebaseTSFirestore} from 'firebasets/firebasetsFirestore/firebaseTSFirestore';
+import { MatDialog } from '@angular/material/dialog';
+import { ReplyComponent } from '../reply/reply.component';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+  @Input() postData!: PostData;
+  creatorName: string = '';
+  creatorDescription: string = '';
 
-  constructor() { }
+  firestore = new FirebaseTSFirestore();
+
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.getCreatorInfo();
+  }
+
+  onReplyClick(){
+    this.dialog.open(ReplyComponent); 
+  }
+
+  openReplyBox(){
+    this.dialog.open(ReplyComponent);
+  }
+
+  getCreatorInfo(){
+    this.firestore.getDocument(
+      {
+        path: ["Users", this.postData.creatorId],
+        onComplete: result => {
+          let userDocument = result.data();
+          if (userDocument) {
+            this.creatorName = userDocument.publicName || 'Unknown';
+            this.creatorDescription = userDocument.description || '';
+          }
+        }
+      }
+    );
   }
 
 }
+
+
